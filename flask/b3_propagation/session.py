15d3f89d8e6084e671edfc50199a5aa518107c1f
kuzaxak <vladimir.kuznichenkov@skybonds.com>
2018-11-19 12:24:04 +0200

    Update session.py
    Implement python2 compatible syntax

--- flask/b3_propagation/session.py
+++ flask/b3_propagation/session.py
@@ -6,5 +6,5 @@ from b3_propagation import get_zipkin_headers
 class ZipkinHeadersSession(Session):
     def request(self, method, url, **kwargs):
         zipkin_headers = get_zipkin_headers()
-        kwargs['headers'] = {**kwargs.get('headers', {}), **zipkin_headers}
-        return super().request(method, url, **kwargs)
+        kwargs['headers'] = kwargs.get('headers', {}).update(zipkin_headers)
+        return super(ZipkinHeadersSession).request(method, url, **kwargs)
